74b5fe0597bd8bc23cc8106d921abc3547a31cc7
pedrog022 <22gordep@gmail.com>
2024-10-04 23:44:00 +0000

    feat: Adiciona usuário básico em AppFixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,33 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
+    private UserPasswordHasherInterface $userPasswordHasher;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
+    public function load(ObjectManager $manager, ): void
     {
         // $product = new Product();
         // $manager->persist($product);
 
+        $user = new User();
+
+        $user->setName('Admin');
+        $user->setEmail('admin@uaifood.com');
+        $user->setPassword($this->userPasswordHasher->hashPassword($user, 'admin'));
+        $user->setRoles(['ROLE_USER']);
+
+        $manager->persist($user);
         $manager->flush();
     }
 }
