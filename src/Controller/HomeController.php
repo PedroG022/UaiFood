1e698ec05df7942775c5399ef9e72bc18bf8c3e2
pedrog022 <22gordep@gmail.com>
2024-10-04 23:44:00 +0000

    feat: Mostra todas lojas na página inicial

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\StoreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,16 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(StoreRepository $storeRepository): Response
     {
-        # Redireciona o usuário se ele não estiver logado
-
-        if (!$this->getUser()) {
-            return $this->redirectToRoute('app_landing');
-        }
+        $stores = $storeRepository->findAll();
 
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'stores' => $stores
         ]);
     }
 }
